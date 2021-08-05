23667cfc547995344522c678b4b9e545b6a747fd
GI-Corp <gicorp.work@gmail.com>
2021-08-05 18:13:53 +0500

    Add API to app folder

---
+++ datum/serializers.py
@@ -0,0 +1,45 @@
+from django.contrib.auth import models
+from django.db.models import fields
+from rest_framework import serializers
+from rest_framework.utils import field_mapping
+from datum.models import User, Profile, Preference, Interest, Match
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length=100)
+    password = serializers.CharField(max_length=100)
+
+    class Meta:
+        fields = ('username', 'password',)
+
+    def create(self, validated_data):
+        raise NotImplementedError
+
+    def update(self, validated_data):
+        raise NotImplementedError
+
+
+class ProfileSerializer(serializers.ModelSerializer):    
+    class Meta:
+        model = Profile
+        fields = '__all__'
+
+class PreferenceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Preference
+        fields = '__all__'
+
+class InterestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Interest
+        fields = '__all__'
+
+class MatchSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Match
+        fields = '__all__'
+
